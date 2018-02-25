e813168834adad79404162019871034336ef10f5
Mário Gil <mg_mesquita@hotmail.com>
2018-02-25 16:08:42 +0000

    Added all the Input Data Sets

--- GilPlayground.py
+++ GilPlayground.py
@@ -1,8 +1,11 @@
 input_file = open("example.in")
-
 first_line = input_file.readline()
+magic_numbers = first_line.split(" ")
 
-thisline = first_line.split(" ")
+n_rows = magic_numbers[0]
+n_columns = magic_numbers[1]
+minimum_ingredients = magic_numbers[2]
+maximum_cells = magic_numbers[3]
 
-print(first_line)
-print(thisline)
+pizza = input_file.readlines()
+print (pizza)

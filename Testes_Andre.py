834cfb04fefce93b2f345bd3a12e6986d86cf75c
andrefmrocha <andrefmrocha@live.com.pt>
2018-02-25 16:36:23 +0000

    Função adicionada em main

--- Testes_Andre.py
+++ Testes_Andre.py
@@ -10,13 +10,18 @@ print(thisline)
 i=1
 ultimo=magic_numbers[3]
 numero=ultimo[0]
-while True:
-    if ultimo[i] == '\n':
-        magic_numbers.pop()
-        magic_numbers.append(ultimo[i])
-        break
-    numero=numero+ultimo[i]
-    i+=1
+def limpar_caracter(lista):
+    i=1
+    tamanho=lista.length
+    ultimo=lista[tamanho]
+    numero=ultimo[0]
+    while True:
+        if ultimo[i] == '\n':
+            magic_numbers.pop()
+            magic_numbers.append(ultimo[i])
+            break
+        numero=numero+ultimo[i]
+        i+=1
 
 
 
--- main.py
+++ main.py
@@ -13,3 +13,16 @@ input_file = open("example.in").read()
 
 pizza = [x for x in input_file if x.isalpha()]
 print (pizza)
+
+def limpar_caracter(lista):
+    i=1
+    tamanho=lista.length
+    ultimo=lista[tamanho]
+    numero=ultimo[0]
+    while True:
+        if ultimo[i] == '\n':
+            magic_numbers.pop()
+            magic_numbers.append(ultimo[i])
+            break
+        numero=numero+ultimo[i]
+        i+=1

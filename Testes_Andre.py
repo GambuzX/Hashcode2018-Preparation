9862a76c898804cec453d2b94c6029b717d780b4
andrefmrocha <andrefmrocha@live.com.pt>
2018-02-25 16:31:13 +0000

    Criada função para remove '\n'

--- Testes_Andre.py
+++ Testes_Andre.py
@@ -1,16 +1,24 @@
-input_file = open("example.in")
+input_file = open("big.in")
 
 first_line = input_file.readline()
 
-thisline = first_line.split(" ")
+magic_numbers = first_line.split(" ")
 
 print(first_line)
 print(thisline)
 
-i=0
+i=1
+ultimo=magic_numbers[3]
+numero=ultimo[0]
 while True:
-    caracter=thisline[i]
-    íf caracter == 'meias':
+    if ultimo[i] == '\n':
+        magic_numbers.pop()
+        magic_numbers.append(ultimo[i])
+        break
+    numero=numero+ultimo[i]
+    i+=1
+
+
 
 
 
